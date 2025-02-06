bc335494ed69303b7a9581441fe1db19deaaa5fb
Jonatan Huens <jonatan.huens@gmail.com>
2025-02-06 12:10:13 -0300

    Estructura de GatherInput, PlayerController y Movimientos 1D Axis

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D m_rigidbody2D;
+    private GatherInput m_gatherInput;
+    private Transform m_transform;
+    [SerializeField] private float speed;
+
+    void Start()
+    {
+        m_gatherInput = GetComponent<GatherInput>();
+        m_transform = GetComponent<Transform>();
+        m_rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        m_rigidbody2D.linearVelocity = new Vector2(speed * m_gatherInput.ValueX, m_rigidbody2D.linearVelocityY);
+    }
+}
