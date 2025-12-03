e1afd96f698619c6d814f02909214cc5103fcdbc
Jonatan Huens <jonatan.huens@gmail.com>
2025-12-03 11:59:22 -0300

    PC/add: Script Pushing Arrow

--- Assets/Scripts/ArrowController.cs
+++ Assets/Scripts/ArrowController.cs
@@ -2,5 +2,16 @@ using UnityEngine;
 
 public class ArrowController : TrampolineController
 {
+    [Header("Additional Info")]
+    [SerializeField] private bool rotationRight;
+    [SerializeField] private float rotationSpeed = 120;
+    private int _direction = -1;
+
+    private void Update()
+    {
+        _direction = rotationRight ? -1 : 1;
+        transform.Rotate(0, 0, (rotationSpeed * _direction) * Time.deltaTime);
+    }
     
+    private void DestroyMe() => Destroy(gameObject);
 }
--- Assets/Scripts/TrampolineController.cs
+++ Assets/Scripts/TrampolineController.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public class TrampolineController : MonoBehaviour
 {
-    private Animator _animator;
+    protected Animator _animator;
     [SerializeField] private float pushPower;
     [SerializeField] private float duration = 0.5f;
 
@@ -19,7 +19,7 @@ public class TrampolineController : MonoBehaviour
         if (player != null)
         {
             player.Push(transform.up * pushPower, duration);
-            _animator.SetTrigger("trampolineActive");
+            _animator.SetTrigger("active");
         }
     }
 }
